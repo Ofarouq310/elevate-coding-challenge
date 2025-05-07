5c79fde25fd7fd8135ab020b706b6aa3c05876a6
Omar Farouq <ofarouq310@gmail.com>
2025-05-07 21:03:26 +0300

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,18 @@
+const printPrime = (a, b) => {
+  for (let i = a; i < b; i++){
+    let flag = false;
+
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag == false) {
+        console.log(i);
+    }
+}
+}
+
+printPrime(7,20);
